db3343f22a6010f4251cb3c10da9b594364a011f
xeeyyal <tu5luzvdh@code.edu.az>
2023-10-17 20:45:49 +0400

    Task18.10 Nullable,Enum,Struct

---
+++ Task18.10/Models/ListInt.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task18._10.Models
+{
+    internal class ListInt
+    {
+        private int[] array;
+        public ListInt(int size)
+        {
+            array = new int[size];
+        }
+
+        public int this[int index]
+        {
+            get 
+            {
+                if (index >= 0 && index < array.Length)
+                    return array[index];
+                else
+                    throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index >= 0 && index < array.Length)
+                    array[index] = value;
+                else
+                    throw new IndexOutOfRangeException();
+            }
+        }
+
+        public void Add(int num)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = num;
+        }
+        public void AddRange(params int[] num)
+        {
+            int Length = array.Length;
+            Array.Resize(ref array, Length + num.Length);
+            Array.Copy(num, 0, array, Length, num.Length);
+        } 
+        public bool Contains(int num) 
+        { 
+            return array.Contains(num);
+        }
+        public int Sum()
+        {
+            return array.Sum();
+        }
+        public void Remove(int num)
+        {
+            if (array.Contains(num))
+            {
+                int[] Array1 = new int[array.Length - 1];
+                int Index1 = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] != num)
+                    {
+                        Array1[Index1] = array[i];
+                        Index1++;
+                    }
+                }
+                array = Array1;
+            }
+        }
+        public void RemoveRange(params int[] nums)
+        {
+            foreach (int num in nums)
+            {
+                if (array.Contains(num))
+                {
+                    int[] Array2 = new int[array.Length - 1];
+                    int Index2 = 0;
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (array[i] != num)
+                        {
+                            Array2[Index2] = array[i];
+                            Index2++;
+                        }
+                    }
+                    array = Array2;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return string.Join(", ", array);
+        }
+    }
+}
---
+++ Task18.10/Program.cs
@@ -0,0 +1,34 @@
+﻿using Task18._10.Models;
+
+namespace Task18._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListInt List = new ListInt(3);
+            List[0] = 1;
+            List[1] = 2;
+            List[2] = 3;
+            List.Add(4);
+            List.AddRange(5, 6, 7);
+            Console.WriteLine("List: " + List.ToString());
+            Console.WriteLine("Sum: " + List.Sum());
+
+            int numToRemove = 4;
+            List.Remove(numToRemove);
+            Console.WriteLine($"{numToRemove} silindi. Yeni list: " + List.ToString());
+
+            int[] numsToRemove = { 2, 6 };
+            List.RemoveRange(numsToRemove);
+            Console.WriteLine(string.Join(", ", numsToRemove) + " silindi. Yeni list: " + List.ToString());
+
+            int index = 3;
+            Console.WriteLine($"Verilmish indexdeki eded {index}: {List[index]}");
+
+            Console.WriteLine($"Contains 3: {List.Contains(3)}");
+            Console.WriteLine($"Contains 12: {List.Contains(12)}");
+
+        }
+    }
+}
